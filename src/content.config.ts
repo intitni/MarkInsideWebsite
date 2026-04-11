45fa338c16031fdd53cec68a0f0262464f1864d7
Shx Guo <int123c@gmail.com>
2026-04-11 15:01:37 +0800

    Update website

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,23 @@
-import { defineCollection } from 'astro:content';
-import { docsLoader } from '@astrojs/starlight/loaders';
-import { docsSchema } from '@astrojs/starlight/schema';
+import { defineCollection, z } from "astro:content";
+import { docsLoader } from "@astrojs/starlight/loaders";
+import { docsSchema } from "@astrojs/starlight/schema";
 
 export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+  docs: defineCollection({
+    loader: docsLoader(),
+    schema: docsSchema({
+      extend: z.object({
+        homePage: z.boolean().optional(),
+        hideHeader: z.boolean().optional(),
+        hideTitle: z.boolean().optional(),
+        redirectSlug: z.string().optional(),
+        changelog: z
+          .object({
+            previousVersion: z.string().optional(),
+            previousTag: z.string().optional(),
+          })
+          .optional(),
+      }),
+    }),
+  }),
 };
